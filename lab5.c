c58804d9394ce1b9af3e6013688243fa43e49776
Rebecca Byun <rebecca_byun@hotmail.com>
2019-04-23 00:59:40 -0500

    added some variables

--- lab5.c
+++ lab5.c
@@ -1,4 +1,6 @@
 // Rebecca Byun | 1000907716 | CSE 2320-004
+// Lab 5 - Depth-First Search
+// Code baselined from Dr. Weems
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,24 +8,45 @@
 #define WHITE 0
 #define GRAY 1
 #define BLACK 2
+
 #define TREE 0
 #define BACK 1
 #define CROSS 2
 #define FORWARD 3
 
-int n;
-int e;
+int n, e;
 
 struct edge {
 	int tail;
 	int head;
 	int type;
+	int dist;
 };
 
 typedef struct edge edgeType;
 edgeType *edgeTab;
 int *firstEdge;
-int *discovery, *finish, *predecessor, *vertexStatus;
+int *discovery, *finish, *predecessor, *successor, *vertexStatus;
+
+void outputTable() {
+	int i;
+	printf("Vertex  discovery  finish  predecessor  successor  longest\n");
+	for (i = 0; i < n; i++) {
+		printf(" %3d      %3d       %3d       %3d       %3d\n", i, discovery[i],finish[i],predecessor[i], successor[i]);
+	}
+	printf("\nEdge\tTail\tHead\tType\tDist\n");
+	printf("----\t----\t----\t----\t----\n");
+	for (i = 0; i < e; i++) {
+  		printf("%d\t%d\t%d\t", i, edgeTab[i].tail, edgeTab[i].head);
+  		switch(edgeTab[i].type) {
+			case TREE:    printf("T\t"); break;
+		//	case BACK:    printf("B\t"); break;		// Shouldn't be any back edges.
+			case CROSS:   printf("C\t"); break;
+			case FORWARD: printf("F\t"); break;
+		}
+		printf("%d\n",edgeTab[i].dist);
+	}
+}
 
 int tailThenHead(const void* xin, const void* yin) {
 	int result;
@@ -35,12 +58,12 @@ int tailThenHead(const void* xin, const void* yin) {
   		return result;
   	}
 	else {
-		return x->head - y->head;
+		return (x->head - y->head);
 	}
 }
 
 void read_input_file() {
-	int a, b, i, j;
+	int a, b, c, i, j;
 	edgeType work;
 	edgeType *ptr;
 
@@ -51,19 +74,20 @@ void read_input_file() {
 		exit(0);
 	}
 	for (i = 0; i < e; i++) {
-		scanf("%d %d", &a, &b);
-		if (a < 0 || a >= n || b < 0 || b >= n) {
-			printf("Invalid input %d %d at %d\n", a, b, __LINE__);
+		scanf("%d %d %d", &a, &b, &c);
+		if (a < 0 || a >= n || b < 0 || b >= n || c < 0) {
+			printf("Invalid input %d %d %d at %d\n", a, b, c, __LINE__);
 			exit(0);
 		}
 		edgeTab[i].tail = a;
 		edgeTab[i].head = b;
+		edgeTab[i].dist = c;
 	}
 
-	// sort edges
+	// Sort edges.
 	qsort(edgeTab, e, sizeof(edgeType), tailThenHead);
 
-	// Coalesce duplicates into a single edge
+	// Coalesce duplicates into a single edge.
 	j = 0;
 	for (i = 1; i < e; i++) {
 		if (edgeTab[j].tail == edgeTab[i].tail && edgeTab[j].head == edgeTab[i].head) {
@@ -77,7 +101,7 @@ void read_input_file() {
 	}
 	e = j+1;
 
-	// For each vertex as a tail, determine first in range of edgeTab entries
+	// For each vertex as a tail, determine first in range of edgeTab entries.
 	firstEdge = (int*)malloc(sizeof(int) * (n+1));
 	if (!firstEdge) {
 		printf("malloc failed %d\n", __LINE__);
@@ -108,14 +132,16 @@ void DFSvisit(int u) {
     	predecessor[v] = u;
     	DFSvisit(v);
 		}
-  		else if (vertexStatus[v]==GRAY) {
-    		edgeTab[i].type=BACK;
+  		else if (vertexStatus[v] == GRAY) {
+    		edgeTab[i].type = BACK;
+    		printf("graph is cyclic\n");
+    		exit(0);
     	}
-    	else if (discovery[u]<discovery[v]) {
-    		edgeTab[i].type=FORWARD;
+    	else if (discovery[u] < discovery[v]) {
+    		edgeTab[i].type = FORWARD;
     	}
   		else {
-	    	edgeTab[i].type=CROSS;
+	    	edgeTab[i].type = CROSS;
 	    }
 	}
 	vertexStatus[u] = BLACK;
@@ -130,46 +156,40 @@ int main () {
 	discovery = (int*)malloc(sizeof(int) * n);
 	finish = (int*)malloc(sizeof(int) * n);
 	predecessor = (int*)malloc(sizeof(int) * n);
+	successor = (int*)malloc(sizeof(int) * n);
 	vertexStatus = (int*)malloc(sizeof(int) * n);
-	if (!discovery || !finish || !predecessor || !vertexStatus) {
+	if (!discovery || !finish || !predecessor || !successor || !vertexStatus) {
 		printf("malloc failed\n");
 		exit(0);
 	}
-	// DFS code
+
+	// Initializing each vertex's color, predecessor, and successor.
 	for (u = 0; u < n; u++) {
 		vertexStatus[u] = WHITE;
 		predecessor[u] = -1;
+		successor[u] = -1;
 	}
+
+	// Beginning the "clock".
 	time = 0;
+
+	// Looking for the next unprocessed vertex to run DFS from.
 	for (u = 0; u < n; u++) {
 		if (vertexStatus[u] == WHITE) {
     		DFSvisit(u);
     	}
     }
 
-	//Output code
-	printf("Vertex  discovery    finish     predecessor\n");
-	for (i = 0; i < n; i++) {
-		printf(" %3d      %3d         %3d          %3d\n", i, discovery[i],finish[i],predecessor[i]);
-	}
-	printf("\n");
-	printf("Edge\tTail\tHead\tType\n");
-	printf("----\t----\t----\t----\n");
-	for (i = 0; i < e; i++) {
-  		printf("%d\t%d\t%d\t", i, edgeTab[i].tail, edgeTab[i].head);
-  		switch(edgeTab[i].type) {
-			case TREE:    printf("tree\n"); break;
-			case BACK:    printf("back\n"); break;
-			case CROSS:   printf("cross\n"); break;
-			case FORWARD: printf("forward\n"); break;
-		}
-	}
+    // Printing end results.
+	outputTable();
 
+	// Exit routine.
 	free(edgeTab);
 	free(firstEdge);
 	free(discovery);
 	free(finish);
 	free(predecessor);
+	free(successor);
 	free(vertexStatus);
 	return 0;
 }
